e97417ef5ef63127a2ad90106508eb2a0b645ae7
Ronny Bangsund <github@neuralshortcircuit.com>
2021-10-31 06:03:47 +0100

    Grouped commands.

--- cmd/umgr/main.go
+++ cmd/umgr/main.go
@@ -14,23 +14,23 @@ import (
 
 var o struct {
 	opt.DefaultHelp
-	Init InitCmd `command:"init" help:"First-time setup."`
+	Init InitCmd `command:"init" help:"First-time setup." group:"Setup commands"`
 
-	ListUsers  ListUsersCmd  `command:"listusers" help:"List all users." aliases:"lu"`
-	CreateUser CreateUserCmd `command:"createuser" help:"Create a new user." aliases:"cu"`
-	DeleteUser DeleteUserCmd `command:"deleteuser" help:"Delete a user." aliases:"du"`
-	// SetPassword SetUPasswordCmd `command:"setpasswd" help:"Set a user's password." aliases:"sup"`
+	ListUsers  ListUsersCmd  `command:"listusers" help:"List all users." aliases:"lu" group:"User commands"`
+	CreateUser CreateUserCmd `command:"createuser" help:"Create a new user." aliases:"cu" group:"User commands"`
+	DeleteUser DeleteUserCmd `command:"deleteuser" help:"Delete a user." aliases:"du" group:"User commands"`
+	// SetPassword SetUPasswordCmd `command:"setpasswd" help:"Set a user's password." aliases:"sup" group:"User commands"`
 
-	ListSites  ListSitesCmd  `command:"listsites" help:"List all sites." aliases:"ls"`
-	CreateSite CreateSiteCmd `command:"createsite" help:"Create a new site." aliases:"cs"`
-	DeleteSite DeleteSiteCmd `command:"deletesite" help:"Delete a site." aliases:"ds"`
+	ListSites  ListSitesCmd  `command:"listsites" help:"List all sites." aliases:"ls" group:"Site commands"`
+	CreateSite CreateSiteCmd `command:"createsite" help:"Create a new site." aliases:"cs" group:"Site commands"`
+	DeleteSite DeleteSiteCmd `command:"deletesite" help:"Delete a site." aliases:"ds" group:"Site commands"`
 
-	// ListGroups  ListGroupsCmd  `command:"listgroups" help:"List all groups." aliases:"lg"`
-	CreateGroup CreateGroupCmd `command:"creategroup" help:"Create a new group." aliases:"cg"`
-	// DeleteGroup DeleteGroupCmd `command:"deletegroup" help:"Delete a group." aliases:"dg"`
-	// AddMember AddMemberCmd `command:"addmember" help:"Add a user to a group." aliases:"am"`
-	// RemoveMember RemoveMemberCmd `command:"removemember" help:"Remove a user from a group." aliases:"rm"`
-	// ListMembers ListMembersCmd `command:"listmembers" help:"List all members of a group." aliases:"lm"`
+	// ListGroups  ListGroupsCmd  `command:"listgroups" help:"List all groups." aliases:"lg" group:"Group commands"`
+	CreateGroup CreateGroupCmd `command:"creategroup" help:"Create a new group." aliases:"cg" group:"Group commands"`
+	// DeleteGroup DeleteGroupCmd `command:"deletegroup" help:"Delete a group." aliases:"dg" group:"Group commands"`
+	// AddMember AddMemberCmd `command:"addmember" help:"Add a user to a group." aliases:"am" group:"Group commands"`
+	// RemoveMember RemoveMemberCmd `command:"removemember" help:"Remove a user from a group." aliases:"rm" group:"Group commands"`
+	// ListMembers ListMembersCmd `command:"listmembers" help:"List all members of a group." aliases:"lm" group:"Group commands"`
 }
 
 func main() {
--- go.mod
+++ go.mod
@@ -5,13 +5,14 @@ go 1.16
 require (
 	github.com/Urethramancer/ansi v0.1.0
 	github.com/Urethramancer/daemon v0.3.0
-	github.com/Urethramancer/signor v0.16.0
+	github.com/Urethramancer/signor v0.17.0
 	github.com/go-chi/chi v1.5.1
 	github.com/grimdork/sweb v0.9.0
 	github.com/grimdork/xos v0.2.1
 	github.com/jackc/pgx/v4 v4.13.0
-	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5
-	golang.org/x/sys v0.0.0-20210817190340-bfb29a6856f2 // indirect
-	golang.org/x/term v0.0.0-20210615171337-6886f2dfbf5b
+	github.com/jackc/puddle v1.1.4 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
+	golang.org/x/sys v0.0.0-20211030160813-b3129d9d1021 // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211
 	golang.org/x/text v0.3.7 // indirect
 )
