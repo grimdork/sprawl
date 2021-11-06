02514260b1657fd17ea577bf3d55b72e4fa22290
Ronny Bangsund <github@neuralshortcircuit.com>
2021-11-06 03:37:12 +0100

    Correct SQL.

--- group.go
+++ group.go
@@ -10,7 +10,11 @@
 
 package sprawl
 
-import "context"
+import (
+	"context"
+
+	"github.com/jackc/pgx/v4"
+)
 
 // Group has a name and site it belongs to.
 type Group struct {
@@ -20,82 +24,61 @@ type Group struct {
 }
 
 // CreateGroup creates a new group.
-func (db *Database) CreateGroup(name string, site string) error {
-	sql := "insert into groups (name, site) values ($1, $2)"
+func (db *Database) CreateGroup(name, site string) error {
+	sql := `insert into groups (name, sid) select $1,s.id from sites s where s.name=$2;`
 	_, err := db.Exec(context.Background(), sql, name, site)
 	return err
 }
 
 // DeleteGroup deletes a group.
-func (db *Database) DeleteGroup(name string) error {
-	sql := "delete from groups where name = $1 limit 1"
-	_, err := db.Exec(context.Background(), sql, name)
+func (db *Database) DeleteGroup(name, site string) error {
+	sql := "delete from groups where name=$1 and sid=(select id from sites where name=$2);"
+	_, err := db.Exec(context.Background(), sql, name, site)
 	return err
 }
 
 // DeleteGroupByID deletes a group by ID.
 func (db *Database) DeleteGroupByID(id int64) error {
-	sql := "delete from groups where id = $1 limit 1"
+	sql := "delete from groups where id = $1;"
 	_, err := db.Exec(context.Background(), sql, id)
 	return err
-}
-
-// GetGroup returns a group by name.
-func (db *Database) GetGroup(name string) (Group, error) {
-	g := Group{}
-	sql := "select id, name, site from groups where name = $1 limit 1"
-	err := db.QueryRow(context.Background(), sql, name).Scan(&g.ID, &g.Name, &g.Site)
-	return g, err
 }
 
 // GetGroupByID returns a group by ID.
 func (db *Database) GetGroupByID(id int64) (Group, error) {
 	g := Group{}
-	sql := "select id, name, site from groups where id = $1 limit 1"
+	sql := "select id, name, site from groups where id=$1 "
 	err := db.QueryRow(context.Background(), sql, id).Scan(&g.ID, &g.Name, &g.Site)
 	return g, err
 }
 
-// GetGroups returns all groups.
-func (db *Database) GetGroups() []Group {
-	sql := "select id, name, site from groups"
-	rows, err := db.Query(context.Background(), sql)
-	if err != nil {
-		return nil
+// GetGroups returns many groups.
+func (db *Database) GetGroups(start, max int64, site string) ([]Group, error) {
+	var err error
+	var rows pgx.Rows
+
+	if site == "" {
+		sql := "select groups.name,sites.name from groups inner join sites on groups.sid=sites.id order by sites.id,groups.id asc limit $1 offset $2;"
+		rows, err = db.Query(context.Background(), sql, max, start)
+	} else {
+		sql := "select groups.name,sites.name from groups inner join sites on groups.sid=sites.id where sites.name=$3 order by sites.id,groups.id asc limit $1 offset $2;"
+		rows, err = db.Query(context.Background(), sql, max, start, site)
 	}
 
-	groups := []Group{}
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+	var groups []Group
 	for rows.Next() {
 		g := Group{}
-		err := rows.Scan(&g.ID, &g.Name, &g.Site)
+		err := rows.Scan(&g.Name, &g.Site)
 		if err != nil {
-			continue
+			return nil, err
 		}
-
 		groups = append(groups, g)
 	}
 
-	return groups
-}
-
-// GetGroupsBySite returns all groups by site.
-func (db *Database) GetGroupsBySite(site string) []Group {
-	sql := "select id, name, site from groups where site = $1"
-	rows, err := db.Query(context.Background(), sql, site)
-	if err != nil {
-		return nil
-	}
-
-	groups := []Group{}
-	for rows.Next() {
-		g := Group{}
-		err := rows.Scan(&g.ID, &g.Name, &g.Site)
-		if err != nil {
-			continue
-		}
-
-		groups = append(groups, g)
-	}
-
-	return groups
+	return groups, nil
 }
