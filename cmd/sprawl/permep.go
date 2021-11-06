36752f141a2a940304e7e5e77c2d2517f3c1518c
Ronny Bangsund <github@neuralshortcircuit.com>
2021-11-06 03:40:40 +0100

    Stubs.

---
+++ cmd/sprawl/permep.go
@@ -0,0 +1,12 @@
+package main
+
+import "net/http"
+
+func (srv *Server) listPermissions(w http.ResponseWriter, r *http.Request) {
+}
+
+func (srv *Server) createPermission(w http.ResponseWriter, r *http.Request) {
+}
+
+func (srv *Server) deletePermission(w http.ResponseWriter, r *http.Request) {
+}
