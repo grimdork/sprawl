31efe0cdef776eb3f84d9574970cdc439a37bf5c
Ronny Bangsund <github@grimdork.net>
2021-11-06 12:17:52 +0100

    Implemented permission creation command.

---
+++ cmd/umgr/cmd_createpermission.go
@@ -0,0 +1,38 @@
+// Copyright (c) 2021 Ronny Bangsund
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+package main
+
+import (
+	"github.com/Urethramancer/signor/opt"
+	"github.com/grimdork/sprawl"
+)
+
+type CreatePermissionCmd struct {
+	opt.DefaultHelp
+	Name string `placeholder:"NAME" help:"Permission name to create."`
+}
+
+// Run the permission creation.
+func (cmd *CreatePermissionCmd) Run(args []string) error {
+	if cmd.Help || cmd.Name == "" {
+		return opt.ErrUsage
+	}
+
+	cfg, err := LoadConfig()
+	if err != nil {
+		return err
+	}
+
+	err = cfg.GetLoginToken()
+	if err != nil {
+		return err
+	}
+
+	_, err = cfg.Post(sprawl.EPCreatePermission, sprawl.Request{
+		"name": cmd.Name,
+	})
+	return err
+}
