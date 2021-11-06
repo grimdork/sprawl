faad2cbac49e3a507b7941fc736d131301669461
Ronny Bangsund <github@neuralshortcircuit.com>
2021-11-06 03:39:46 +0100

    Implemented group delete.

--- cmd/umgr/cmd_deletegroup.go
+++ cmd/umgr/cmd_deletegroup.go
@@ -4,3 +4,37 @@
 //	https://opensource.org/licenses/MIT
 
 package main
+
+import (
+	"github.com/Urethramancer/signor/opt"
+	"github.com/grimdork/sprawl"
+)
+
+type DeleteGroupCmd struct {
+	opt.DefaultHelp
+	Name string `placeholder:"NAME" help:"Name of group to delete."`
+	Site string `placeholder:"site" help:"Site of the group."`
+}
+
+// Run the group deletion.
+func (cmd *DeleteGroupCmd) Run(args []string) error {
+	if cmd.Help || cmd.Name == "" {
+		return opt.ErrUsage
+	}
+
+	cfg, err := LoadConfig()
+	if err != nil {
+		return err
+	}
+
+	err = cfg.GetLoginToken()
+	if err != nil {
+		return err
+	}
+
+	_, err = cfg.Post(sprawl.EPDeleteGroup, sprawl.Request{
+		"name": cmd.Name,
+		"site": cmd.Site,
+	})
+	return err
+}
