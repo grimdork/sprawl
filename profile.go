b479c682dadf9c9e318fe2d468e750e17cb4cd65
Ronny Bangsund <github@grimdork.net>
2021-11-07 15:42:27 +0100

    Created profile SQL.

--- profile.go
+++ profile.go
@@ -5,20 +5,43 @@
 
 package sprawl
 
+import "context"
+
 // Prfile for a user+site combination.
 type Profile struct {
+	// Site the profile is for.
+	Site string
 	// Data is a site-specific JSON structure.
 	Data string
-	// Groups and permissions for this site.
-	Groups map[string]int64
+	// Groups for this site.
+	Groups map[string]interface{}
 }
 
-// AddMembership adds a user to a site.
-func (db *Database) AddMembership(username, site string) error {
-	return nil
+// CreateProfile both adds a user to a site and creates its site-specific profile entry.
+func (db *Database) CreateProfile(username, site, data string) error {
+	sql := `insert into  profiles (uid,sid,data)
+	values (
+		(select users.id from users where users.name=$1),
+		(select sites.id from sites where sites.name=$2),
+		$3);`
+	_, err := db.Exec(context.Background(), sql, username, site, data)
+	return err
 }
 
-// RemoveMembership removes a user from a site.
-func (db *Database) RemoveMembership(username, site string) error {
-	return nil
+// UpdateProfile updates the profile for a user+site combination.
+func (db *Database) UpdateProfile(username, site, data string) error {
+	sql := `update profiles set data=$3 where
+	uid=(select users.id from users where users.name=$1) and
+	sid=(select sites.id from sites where sites.name=$2);`
+	_, err := db.Exec(context.Background(), sql, username, site, data)
+	return err
+}
+
+// RemoveProfile removes a user from a site.
+func (db *Database) RemoveProfile(username, site string) error {
+	sql := `delete from profiles where
+	uid=(select users.id from users where users.name=$1) and
+	sid=(select sites.id from sites where sites.name=$2);`
+	_, err := db.Exec(context.Background(), sql, username, site)
+	return err
 }
